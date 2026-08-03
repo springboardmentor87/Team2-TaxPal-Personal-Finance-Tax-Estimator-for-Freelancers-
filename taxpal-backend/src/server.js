c2acac3fa9f5bab03ded83310250546ebb8b7535
Bharathsai-07 <bharathsainerella07@gmail.com>
2026-08-03 20:51:08 +0530

    resolved conflict

--- taxpal-backend/src/server.js
+++ taxpal-backend/src/server.js
@@ -1,6 +1,6 @@
 const path = require("path");
-const dotenv = require("dotenv");
 const express = require("express");
+const dotenv = require("dotenv");
 
 dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
