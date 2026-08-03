28d3851bd1ca9b7f7a747fed629e56811d0fd43b
Bharathsai-07 <bharathsainerella07@gmail.com>
2026-08-03 20:49:27 +0530

    fixed the dotenv

--- taxpal-backend/src/server.js
+++ taxpal-backend/src/server.js
@@ -1,6 +1,6 @@
 const path = require("path");
-const express = require("express");
 const dotenv = require("dotenv");
+const express = require("express");
 
 dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
